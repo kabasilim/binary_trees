7fa6b91f1d94651feebb0e7857ab70a6f9619953
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-11-02 17:54:24 -0700

    16-binary_tree_is_perfect.c

--- binary_trees.h
+++ binary_trees.h
@@ -50,5 +50,8 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+size_t count_height(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
 
 #endif
