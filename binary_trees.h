5c42533b80b59488517dff20bdd387ca736e89e6
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-11-03 05:49:24 -0700

    all

--- binary_trees.h
+++ binary_trees.h
@@ -56,6 +56,4 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 int binary_tree_height(const binary_tree_t *tree);
 int binary_tree_is_avl(const binary_tree_t *tree);
 bst_t *bst_remove(bst_t *root, int value);
-bst_t *bst_find_successor(bst_t *node);
-
 #endif
